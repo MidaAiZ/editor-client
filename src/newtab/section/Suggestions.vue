e5271618fdf501fb261a75dd35493e56146a48c7
KingOfSolo <944125893@qq.com>
2019-09-16 18:48:06 +0800

    轮播图

--- src/newtab/section/Suggestions.vue
+++ src/newtab/section/Suggestions.vue
@@ -1,11 +1,45 @@
 <template>
-
+    <div id="suggestions">
+        <swiper :options="swiperOption">
+            <swiper-slide>777</swiper-slide>
+            <swiper-slide>777777</swiper-slide>
+            <div class="swiper-pagination" slot="pagination"></div>
+            <!--<swiper-slide v-for="page in Math.ceil(totalSize/(rowNumber*itemNumber))">-->
+                <!--<el-row :gutter="40" v-for="line in rowNumber">-->
+                    <!--<suggestion-item v-for="item in totalSize-rowNumber*itemNumber-(line-1)*itemNumber>itemNumber?-->
+                    <!--itemNumber:(totalSize-rowNumber*itemNumber-(line-1)*itemNumber)%itemNumber">-->
+                    <!--</suggestion-item>-->
+                <!--</el-row>-->
+            <!--</swiper-slide>-->
+        </swiper>
+    </div>
 </template>
 <script>
-
+import SuggestionItem from '../component/SuggestionItem.vue'
+import { swiper, swiperSlide} from 'vue-awesome-swiper'
 export default {
     name: 'suggestions',
+    components: {
+        SuggestionItem,
+        swiper,
+        swiperSlide
+    },
+    data(){
+        return{
+            rowNumber: 2,
+            itemNumber: 5,
+            totalSize: 15,
+            swiperOption: {
+                pagination: {
+                    el: '.swiper-pagination'
+                }
+            }
+        }
+    }
 }
 </script>
 <style scoped>
+    #suggestions{
+        margin-top: 60px;
+    }
 </style>
