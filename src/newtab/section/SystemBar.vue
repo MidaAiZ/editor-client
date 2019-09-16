eddb6fd11bcf8fe4c5d80454171731237a303727
MiRECoFu <1027066037@qq.com>
2019-09-16 15:19:11 +0800

    添加hover动画

--- src/newtab/section/SystemBar.vue
+++ src/newtab/section/SystemBar.vue
@@ -1,7 +1,6 @@
 <template>
-    <div class="open-system-bar-btn">
-        <el-button @click="drawer = true" type="primary" style="margin-left: 16px;">
-            点我打开
+    <div>
+        <el-button class="open-system-bar-btn" @click="drawer = true" icon="el-icon-menu" size="medium" circle>
         </el-button>
         <el-drawer
             title="我是标题"
@@ -33,7 +32,18 @@ export default {
 <style scoped>
     .open-system-bar-btn {
         position: fixed;
-        right: 20px;
-        top: 20px;
+        right: 25px;
+        top: 25px;
+        border: 0px;
+        background-color: #555;
+        opacity: 0.5;
+        color: #fff;
+        transition: all 0.3s;
+    }
+    .open-system-bar-btn:hover {
+        transform: scale(1.1);
+        background-color: #555;
+        opacity: 0.5;
+        color: #fff;
     }
 </style>
