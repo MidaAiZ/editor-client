262a1434bb14a94b93b768e631aa39d34c37fcf1
MiRECoFu <1027066037@qq.com>
2019-09-16 13:12:02 +0800

    确定文件结构

--- src/newtab/root.vue
+++ src/newtab/root.vue
@@ -1,17 +1,38 @@
 <template>
-  <section id="newtab-page">
-
-  </section>
+  <v-app>
+    <v-content>
+      <background />
+      <systemBar />
+      <section>
+        <v-layout>
+          <v-flex>
+            <customSearch />
+            <suggestions />
+          </v-flex>
+        </v-layout>
+      </section>
+    </v-content>
+      
+  </v-app>
 </template>
 <script>
+import background from '../section/Background'
+import systemBar from '../section/SystemBar'
+import suggestions from '../section/Suggestions'
+import customSearch from '../section/CustomSearch'
+
 export default {
-    name: 'root'
+    name: 'root',
+    components: {
+    systemBar,
+    background,
+    suggestions,
+    customSearch,
+  }
 }
 </script>
 <style scoped>
 ::-webkit-scrollbar { 
   display: none; 
-}
-#newtab-page {
 }
 </style>
