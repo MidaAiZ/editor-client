b3c1859e17be4579cf77056d9c04f254fce9016d
MiRECoFu <1027066037@qq.com>
2019-09-24 20:38:53 +0800

    添加api配置文件

---
+++ src/newtab/services/index.js
@@ -0,0 +1,15 @@
+// const baseURL = '//'
+
+
+const APIS = {
+    wallPaper: {
+        Bing: {
+            default: {
+                p: `${BingWPBaseURL}&idx=0&n=1&mkt=zh-CN`
+            },
+
+        },
+    }
+}
+
+export default APIS
