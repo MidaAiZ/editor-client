7e56e09109fe27e29f7f5c66ec220c04d0b97c8c
MiRECoFu <1027066037@qq.com>
2019-10-06 04:25:38 +0800

    添加书签栏功能

---
+++ src/newtab/section/Bookmarks.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+    </div>
+</template>
+<script>
+// import './newBGbrush.scss'
+import { mapGetters, mapState, mapMutations, mapActions } from 'vuex'
+
+export default {
+    name: 'bookmarks',
+    components: {
+        // newBgBrush,
+    },
+    data(){
+        return{
+        }
+    },
+    computed: {
+        
+    },
+    methods: {
+        
+    }
+}
+</script>
+<style scoped>
+    
+</style>
