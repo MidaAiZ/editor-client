c1d09dcb3bf71031bc481e7c6ab8f8ca19612dd0
MiRECoFu <1027066037@qq.com>
2019-11-17 01:39:27 +0800

    天气

---
+++ src/newtab/section/Weather.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+<script>
+import { mapState, mapMutations } from 'vuex'
+import localeText from '../../../static/locale/index.js'
+
+
+export default {
+    name: 'weather',
+    components: {
+        barTab,
+        barSearch,
+        webList,
+    },
+    data() {
+      return {
+      };
+    },
+    computed: {
+        
+    },
+    created: function() {
+    },
+    methods: {
+    }
+}
+</script>
+<style scoped>
+</style>
