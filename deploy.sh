077764920b70a9342f080a9a4bd2880a2231029f
SinnerAiZ <1036570699@qq.com>
2020-04-25 23:22:06 +0800

    feat:update project config

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "正在打包..."
+file="dist"
+rm -rf $file
+npm run build
+echo "上传目标文件${file}"
+ssh root@139.196.120.93 "cd ./tabplus-client;rm -rf ${file}"
+sftp root@139.196.120.93 <<EOF
+put -r $file tabplus-client/${file}
+EOF
+
+echo "程序执行完毕"
+
