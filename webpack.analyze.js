ffe8389511dcfe13b092e28405b40142d125b937
SinnerAiZ <1036570699@qq.com>
2020-04-25 23:25:23 +0800

    feat:update build command

--- webpack.analyze.js
+++ webpack.analyze.js
@@ -1,7 +1,7 @@
 const merge = require('webpack-merge');
 const { BundleAnalyzerPlugin }  = require('webpack-bundle-analyzer');
 const prodConfig = require('./webpack.prod.js');
-
+ 
 module.exports = merge(prodConfig, {
   plugins: [
     new BundleAnalyzerPlugin(),
--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,7 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
+
 module.exports = merge(common, {
    mode:'production',
    plugins: [
